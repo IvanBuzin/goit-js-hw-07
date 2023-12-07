8044f44de3d95f6ea75ff2c275757c4bb7d0d255
IvanBuzin <ua.buzin@gmail.com>
2023-12-07 23:25:20 +0200

    zxc

--- js/task-01.js
+++ js/task-01.js
@@ -8,5 +8,65 @@ items.forEach(item => {
   const elements = item.querySelectorAll('li').length;
 
   console.log(`Category: ${title}
-  Elements: ${elements}`);
+  Elements: ${elements}`
+  );
 });
+
+const categoriesRef = document.querySelector(".categories");
+categoriesRef.classList.add("js-categories");
+const headRef = document.querySelector("head");
+
+headRef.insertAdjacentHTML(
+  "beforeend",
+  `<stile>
+  .js-categories {
+  display: inline-flex;
+  flex-direction: column;
+  gap: 24px;
+  border-radius: 8px;
+  background: #fff;
+  width: 392px;
+  height: 840px;
+  padding: 24px;
+  border-radius: 8px;
+  color: rgba(128, 128, 128, 1);
+justify-content: left;
+}
+.js-categories > li.item {
+display: flex;
+padding: 16px;
+flex-direction: column;
+gap: 16px;
+ justify-content: left;
+ border-radius: 8px;
+background: #F6F6FE;
+}
+.js-categories > li.item > h2 {
+color: #2e2f42;
+  font-family: Montserrat;
+  font-size: 24px;
+  font-style: normal;
+  font-weight: 600;
+  line-height: 32px; /* 133.333% */
+  letter-spacing: 0.96px;  
+}
+.js-categories > li.item > ul {
+  display: flex;
+flex-direction: column;
+gap: 8px;
+padding-inline-start: 0px;
+}
+.js-categories > li.item > ul > li {
+  width: 360px;
+height: 40px;
+flex-shrink: 0;
+border-radius: 4px;
+border: 1px solid #808080;
+color: rgba(128, 128, 128, 1);
+}
+</stile>`
+);
+
+//<link rel="preconnect" href="https://fonts.googleapis.com">
+//<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+//<link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600&family=Roboto:wght@400;500;700&display=swap" rel="stylesheet"></link>
